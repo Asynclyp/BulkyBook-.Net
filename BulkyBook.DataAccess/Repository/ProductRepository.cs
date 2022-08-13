7ee86b0f59b1c67f28036ea9806240c195b7c227
Asynclyp <ldzlyp770456668@gmail.com>
2022-08-12 23:36:06 -0500

    Product table created. Controller in progress.

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) :base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDd = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDd != null)
+            {
+            
+                objFromDd.Title = obj.Title;
+                objFromDd.ISBN = obj.ISBN;
+                objFromDd.Price = obj.Price;
+                objFromDd.Price50 = obj.Price50;
+                objFromDd.ListPrice = obj.ListPrice;
+                objFromDd.Price100 = obj.Price100;
+                objFromDd.Description = obj.Description;
+                objFromDd.CategoryId = obj.CategoryId;
+                objFromDd.Author = obj.Author;
+                objFromDd.CoverTypeId = obj.CoverTypeId;
+                if(obj.ImageUrl != null)
+                {
+                    objFromDd.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BulkyBook.DataAccess;
+using BulkyBook.Models;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+
+        
+
+
+
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }
+            );
+
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }
+            );
+
+            if (id == null || id == 0)
+            {
+                //Create Product
+                ViewBag.CategoryList = CategoryList;
+                return View(product);
+            }
+            else
+            {
+                //Update Product
+            }
+           
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType obj)
+        {
+            //Server side validation
+            //if (obj.Name == obj.Name.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The DisplayOrder cannot be exactly match the Name.");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Cover Type successfully Updated!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //var categoryFromDb = _db.Categories.Find(id);
+            var coverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (coverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+            return View(coverTypeFromDbFirst);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Cover Type successfully deleted!";
+            return RedirectToAction("Index");
+
+
+        }
+
+
+
+
+    }
+}
